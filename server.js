d798109475d163401ff0c3fbcf3302d7e9a2182e
Stanley <AngSanley@users.noreply.github.com>
2019-09-13 20:29:28 +0700

    Enable CORS

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ let app = express();
 let api = require('./api');
 let bodyParser = require('body-parser');
 let routes = require('./routes');
+let cors = require('cors');
 let apiUrl = "/api/v1";
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.listen(8000, '0.0.0.0');
