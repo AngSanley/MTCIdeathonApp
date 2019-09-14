d916fcdaa8d93150df420b3da48626352d600218
brianwo <45139213+brianwo@users.noreply.github.com>
2019-09-14 12:30:02 +0700

    Improved UI

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,5 +1,3 @@
-const moment = require("moment");
-
 //Code with 💗 in my kost
 
 let jeson = '{ "status":200,"response":[ { "task_id":1,"task_name":"Pengumpulan Proposal",'+
@@ -9,27 +7,33 @@ let jeson = '{ "status":200,"response":[ { "task_id":1,"task_name":"Pengumpulan 
           '"task_name":"Bersihin Kotoran Kucing di Karpet", "task_description":"Tolong itu eek kucingnya dibuang.",'+
           '"task_accept_submission":0,"task_date_from":"2019-09-13T00:00:00.000Z",'+
           '"task_date_to":"2019-09-15T00:00:00.000Z"}]}';
-
-console.log(jeson);
 let obj = JSON.parse(jeson);
 
 if(obj.status == 200) obj.response.forEach(addchild);
 
 function addchild(item,index) {
     document.getElementById("task_parent").innerHTML +=
-    '<div class="task_content"><h3>'+item.task_name+'</h3>'+item.task_description+
+    '<div class="task_content"><h3>'+item.task_name+'</h3><hr>'+item.task_description+
     submission(item.task_accept_submission,item.task_date_to)+'</div>';
 }
-
 
 function submission(validation,deadline){
     var date = new Date(deadline);
 
-    var submission_date = date.getDay() + date.getDate() + date.getMonth() + date.getFullYear();
+    var submission_date = day(date.getDay()) + ", " + String(date.getDate()) + " " + month(date.getMonth()) + " " + String(date.getFullYear());
 
     if(validation == 1){
-        return '<div class="submission"><button class="button" style="margin-right:16px">Upload</button> Deadline: '+submission_date+'</div>';
+        //for some reason, use client-side validation
+        if(date.getTime()<=Date.now()){
+            return '<div class="submission"><button class="button" style="margin-right:16px" disabled>Upload</button> Deadline: '+submission_date+'</div>';
+        }
+        else return '<div class="submission"><button class="button" style="margin-right:16px">Upload</button> Deadline: '+submission_date+'</div>';
     }
+
+    // else if(validation == 2){
+    // return '<div class="submission"><button class="button" style="margin-right:16px">Upload</button> Deadline: '+submission_date+'</div>';
+    // }
+    
     else{
         return '';
     }
